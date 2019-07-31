4b3672d338dd29e3a23cba98364680905eaad553
jindalabhinav <abhinav_jindal@dell.com>
2019-07-31 14:26:23 +0530

    Database Migration 1.3

--- test/PalTrackerTests/TimeEntryIntegrationTest.cs
+++ test/PalTrackerTests/TimeEntryIntegrationTest.cs
@@ -15,10 +15,14 @@ namespace PalTrackerTests
 
         private readonly HttpClient _testClient;
 
+
         public TimeEntryIntegrationTest()
         {
+            Environment.SetEnvironmentVariable("MYSQL__CLIENT__CONNECTIONSTRING", DbTestSupport.TestDbConnectionString);
+            DbTestSupport.ExecuteSql("TRUNCATE TABLE time_entries");
             _testClient = IntegrationTestServer.Start().CreateClient();
         }
+
 
         [Fact]
         public void Read()
